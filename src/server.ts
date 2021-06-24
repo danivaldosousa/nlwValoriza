4d0d25a26d7c90e9b863c0ea9c2da66b30dedf1f
danivaldosousa <danivaldo@gmail.com>
2021-06-24 13:05:56 -0300

    Three commint with Tags

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express, { response, request } from "express";
+import express, { Response, Request, NextFunction, response } from "express";
+import "express-async-errors"
 import { router } from "./routes";
 import "./database";
 
@@ -7,5 +8,17 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+});
 
 app.listen(3000, () => console.log("Server is running"));
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -5,9 +5,7 @@ interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
-
 }
-
 class CreateUserService {
   async execute({ name, email, admin }: IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
